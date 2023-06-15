c8229581a4ee66dce80aad959836363ec4cd4319
Daniil Volozhaninov <Volozhaninovd@mail.ru>
2023-06-15 15:56:52 +0500

    Add Automapper + Setup ActionFilters

---
+++ BookingsWithMe/ActionFilter/ErrorModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BookingsWithMe.ActionFilter;
+
+public class ErrorModel
+{
+    public string FieldName { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+}
---
+++ BookingsWithMe/ActionFilter/ErrorResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace BookingsWithMe.ActionFilter;
+
+public class ErrorResponse
+{
+    public List<ErrorModel> Errors { get; set; } = new();
+}
